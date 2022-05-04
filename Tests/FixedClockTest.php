47d2e72a0b7bcf0e5ca6e9493de3561be7a6721d
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2022-05-04 08:30:07 -0400

    moving stuff around

---
+++ Tests/FixedClockTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SonsOfPHP\Component\Clock\Tests;
+
+use SonsOfPHP\Component\Clock\ClockException;
+use SonsOfPHP\Component\Clock\ClockInterface;
+use SonsOfPHP\Component\Clock\FixedClock;
+use PHPUnit\Framework\TestCase;
+use DateTimeZone;
+
+final class FixedClockTest extends TestCase
+{
+    public function testItHasTheCorrectInterface(): void
+    {
+        $clock = new FixedClock();
+
+        $this->assertInstanceOf(ClockInterface::class, $clock);
+    }
+
+    public function testTheDefaultTimezoneIsUTC(): void
+    {
+        $clock = new FixedClock();
+        $this->assertSame('UTC', $clock->getZone()->getName());
+    }
+
+    public function testSettingTheTimezoneInTheConstructorWorks(): void
+    {
+        $clock = new FixedClock(new DateTimeZone('America/New_York'));
+        $this->assertSame('America/New_York', $clock->getZone()->getName());
+    }
+
+    public function testNowRemainsTheSame(): void
+    {
+        $clock = new FixedClock();
+        $tickOne = $clock->now();
+        $tickTwo = $clock->now();
+        $this->assertSame($tickOne, $tickTwo);
+    }
+
+    public function testTickChangesTime(): void
+    {
+        $clock = new FixedClock();
+        $tickOne = $clock->now();
+        usleep(100);
+        $clock->tick();
+        $tickTwo = $clock->now();
+        $this->assertTrue($tickOne < $tickTwo);
+    }
+
+    public function testSetTimeWithTickTo(): void
+    {
+        $clock = new FixedClock();
+        $clock->tickTo('2020-01-01 00:00:00');
+        $tick = $clock->now();
+        $this->assertSame('2020-01-01 00:00:00', $tick->format('Y-m-d H:i:s'));
+    }
+
+    public function testTickToThrowsExceptionWithInvalidInput(): void
+    {
+        $clock = new FixedClock();
+
+        $this->expectException(ClockException::class);
+        $clock->tickTo('20');
+    }
+
+    public function testToStringMagicMethod(): void
+    {
+        $clock = new FixedClock();
+        $this->assertSame('FixedClock[UTC]', (string) $clock);
+    }
+}
